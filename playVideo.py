552c6ab7238fc6aff668a89202db3d24bfd6d858
nagarjun-repala <nagarjun@192.168.1.5>
2022-04-26 22:33:46 +0530

    Python Selenium project

--- playVideo.py
+++ playVideo.py
@@ -37,10 +37,10 @@ videoPanel = driver.find_element(By.CSS_SELECTOR, '#react-aria-modal-dialog')
 ActionChains(driver).move_to_element(videoPanel).double_click().perform()
 logging.info("Full screen mode for video")
 
-#Getting title of the video
-getTitle = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/section[1]/div[1]/div[1]/div[2]/div/h1')
-logging.info("Title of the video: " + getTitle.get_attribute('textContent'))
+#Getting description of the video
+getDescription = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/section[1]/div[1]/div[1]/div[2]/div/div[2]/div[1]')
+logging.info("Description of the video: " + getDescription.get_attribute('textContent'))
 
 #Getting cast of the video
 getCast = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/section[1]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div[1]/span[2]')
-logging.info("Movie cast: " + getCast.get_attribute('textContent'))
+logging.info("Movie starcast: " + getCast.get_attribute('textContent'))
